cd3fed59a61c6f57f22ca7e9b0cc9cffa8b57fc3
PrpgramTraveler <2115655391@qq.com>
2023-04-20 19:53:42 +0800

    2023-04-20

---
+++ parking_planning_ws/src/planning/src/init_pose_subscriber.cpp
@@ -0,0 +1,23 @@
+#include "planning/init_pose_subscriber.h"
+
+InitPoseSubscriber2D::InitPoseSubscriber2D(ros::NodeHandle &nh, const std::string &topic_name, size_t buff_size) {
+    subscriber_ = nh.subscribe(topic_name, buff_size, &InitPoseSubscriber2D::MessgeCallBack, this);
+}
+
+void InitPoseSubscriber2D::MessgeCallBack(const geometry_msgs::PoseWithCovarianceStampedPtr &init_pose_ptr) {
+    buff_mutex_.lock();
+    init_poses_.emplace_back(init_pose_ptr);
+    buff_mutex_.unlock();
+}
+
+void InitPoseSubscriber2D::ParseData(std::deque<geometry_msgs::PoseWithCovarianceStampedPtr> &pose_data_buff) {
+    buff_mutex_.lock();
+
+    if (!init_poses_.empty()) {
+        pose_data_buff.insert(pose_data_buff.end(), init_poses_.begin(), init_poses_.end());
+
+        init_poses_.clear();
+    }
+
+    buff_mutex_.unlock();
+}
